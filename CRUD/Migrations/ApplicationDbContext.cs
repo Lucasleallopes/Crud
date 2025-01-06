7077b00db988b75d66c299ce24bc8d5ff1ee606c
Lucasleallopes <lucasrodrigoleallopes2014@gmail.com>
2025-01-06 12:50:29 -0300

    adicionando arquivos e pastas

---
+++ CRUD/Migrations/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Migrations {
+    public class ApplicationDbContext : DbContext {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            var connectionString = "Server=localhost;Database=BancoLucas;User=root;Password=root;";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Item>().ToTable("Items");
+        }
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Migrations;
+using CRUD.Repository;
+using CRUD.Controllers;
+
+namespace CRUD {
+    public static class Program {
+        public static async Task Main(string[] args) {
+            await using var context = new ApplicationDbContext();
+            var repository = new ItemRepository(context);
+            var controller = new ItemController(repository);
+            await controller.Run();
+        }
+    }
+}
